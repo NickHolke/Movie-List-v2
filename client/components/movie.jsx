cde82db2d27cd256d99542a0f609bce302847502
Nick Holke <nickholke@gmail.com>
2019-11-23 15:44:26 -0800

    Added css styling

---
+++ client/components/movie.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Movie = ({movieData}) => (
+    <div id='movie'><span id="title">{movieData.title}</span></div>
+)
+
+export default Movie;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: './client/index.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/public'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+}
