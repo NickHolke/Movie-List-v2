0fc9b7b90ce53a9bd539423452671321200b8644
Nick Holke <nickholke@gmail.com>
2019-11-23 21:52:56 -0800

    Receive data from api request and handle no movie data response

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Search from './search.jsx';
 import Tabs from './tabs.jsx';
 import AddMovie from './addMovie.jsx';
+import axios from 'axios';
+
+const key = '1aa4b71d59342b08b19dea8b16bcf4aa';
+const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=`
+
 
 class App extends React.Component {
     constructor(props) {
@@ -10,7 +15,8 @@ class App extends React.Component {
         this.state = {
             movies : [
             ],
-            tab: 'All Movies'
+            tab: 'All Movies',
+            notFound: null
         }
 
         this.searchHandler = this.searchHandler.bind(this);
@@ -32,24 +38,49 @@ class App extends React.Component {
     }
 
     inputHandler(title) {
-        let movie = {
-            title: title,
-            display: true,
-            watched: false
-        }
-        this.setState((state) => {
-             return {
-                movies: state.movies.concat([movie])
-            }
-        })   
+        title = encodeURI(title);
+
+        axios.get(url + title)
+        .then((response) =>  {
+            if (response.data.results.length === 0) {
+                this.setState({notFound: 'No movie found by that name'})
+            } else {
+                let data = response.data.results[0];
+                let movie = {
+                    id: data.id,
+                    title: data.title,
+                    overview: data.overview,
+                    vote_average: data.vote_average,
+                    release_date: data.release_date,
+                    poster_path: 'http://image.tmdb.org/t/p/w185/' + data.poster_path,
+                    display: true,
+                    watched: false
+                }
+    
+                this.setState((state) => {
+                    return {
+                       movies: state.movies.concat([movie]),
+                       notFound: null
+                   }
+               })  
+            } 
+        })
+        .catch((err) => console.log(err))
+        
     }
 
     tabHandler(e) {
-        this.setState({tab: e.target.name})
+        let newMovies = this.state.movies.map((movie) => {
+            movie.display = true;
+            return movie;
+        })
+        this.setState({
+            movies: newMovies,
+            tab: e.target.name
+        })
     }
 
     watchedHandler(movieData) {
-        //console.log(movieData)
         let newMovies = this.state.movies.map((movie) => {
             if (movie.title === movieData.title) {
                 movie.watched = !movie.watched;
@@ -65,7 +96,8 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <AddMovie inputHandler={this.inputHandler}/>
+                
+                <AddMovie inputHandler={this.inputHandler}/>{this.state.notFound ? this.state.notFound : ""} 
                 <Search searchHandler={this.searchHandler}/>
                 <button className={this.state.tab === "All Movies" ? "tabs selected" : "tabs"} name="All Movies" onClick={this.tabHandler}>All Movies</button>
                 <button className={this.state.tab === "Watched" ? "tabs selected" : "tabs"} name="Watched" onClick={this.tabHandler}>Watched</button>
--- client/components/movieInfo.jsx
+++ client/components/movieInfo.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const MovieInfo = ({movieData, watchedHandler}) => (
-    <div>
-        <button className={movieData.watched ? "selected" : ""} 
+    <div id="movie-info">
+        <img id="poster" src={movieData.poster_path}></img>
+        <div id="overview"><b>Overview:</b> {movieData.overview}</div>
+        <div><b>Vote Average:</b> {movieData.vote_average}</div> 
+        <div><b>Release Date: </b>{movieData.release_date}</div>
+        <button id="watched-button"className={movieData.watched ? "selected" : ""} 
                 onClick={() => watchedHandler(movieData)}>Watched
         </button>
     </div>
